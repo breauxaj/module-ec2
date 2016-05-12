cd36aab7fe1e3cd52847fd234fd3b99c2b14c6db
Adam Breaux <breauxaj@gmail.com>
2016-05-12 19:11:33 -0400

    needs work

---
+++ manifests/init.pp
@@ -0,0 +1,34 @@
+# Class: aws
+#
+# This class installs aws packages
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the list of packages
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'aws':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'aws':
+#      ensure => 'absent'
+#    }
+#
+class aws (
+  $ensure = 'latest'
+) inherits ::aws::params {
+  package { $::aws::params::aws_package:
+    ensure  => $ensure,
+  }
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,10 @@
+# Class: aws::params
+#
+# This class sets parameters used in this module
+#
+# Actions:
+#   - Defines numerous parameters used by other classes
+#
+class aws::params {
+
+}
