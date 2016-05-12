34fbff645bbdc287f68648ae2e46d29e48db3a31
Adam Breaux <breauxaj@gmail.com>
2016-05-12 19:34:20 -0400

    setup the clause to protect this module from running on non-aws nodes

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,16 @@
 #
 # Actions:
 #   - Defines numerous parameters used by other classes
+#   - Does not support other operatingsystem patterns - amazon only
 #
 class aws::params {
+  case $::operatingsystem {
+    'amazon': {
+      
+    }
+    default: {
+      fail("Unsupported platform: ${::operatingsystem}")
+    }
+  }
 
 }
