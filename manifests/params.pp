c41c0f728ac6385273aec5d9c545fcd8c9909bc4
Adam Breaux <breauxaj@gmail.com>
2016-05-12 20:09:36 -0400

    maintain these packages

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,19 @@
 class aws::params {
   case $::operatingsystem {
     'amazon': {
-      
+      $aws_packages = [
+        'aws-amitools-ec2',
+        'aws-apitools-as',
+        'aws-apitools-common',
+        'aws-apitools-ec2',
+        'aws-apitools-elb',
+        'aws-apitools-mon',
+        'aws-cfn-bootstrap',
+        'aws-cli',
+        'ec2-net-utils',
+        'ec2-utils'
+      ]
+
     }
     default: {
       fail("Unsupported platform: ${::operatingsystem}")
