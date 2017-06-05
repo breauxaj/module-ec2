15b0d181420fff6f5f80dca02c73905b8c9fe7f1
Adam Breaux <breauxaj@gmail.com>
2017-06-05 17:58:59 -0400

    can’t fully test with the local ami, but generally worked

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,23 +9,34 @@
 class ec2::params {
   $ec2_package_ensure = 'latest'
 
-  case $::operatingsystem {
-    'Amazon': {
-      $ec2_packages = [
-        'aws-amitools-ec2',
-        'aws-apitools-as',
-        'aws-apitools-common',
-        'aws-apitools-ec2',
-        'aws-apitools-elb',
-        'aws-apitools-mon',
-        'aws-cfn-bootstrap',
-        'aws-cli',
-        'ec2-net-utils',
-        'ec2-utils'
-      ]
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystem {
+        'Amazon': {
+          case $::operatingsystemmajrelease {
+            default: {
+              $ec2_packages = [
+                'aws-amitools-ec2',
+                'aws-apitools-as',
+                'aws-apitools-common',
+                'aws-apitools-ec2',
+                'aws-apitools-elb',
+                'aws-apitools-mon',
+                'aws-cfn-bootstrap',
+                'aws-cli',
+                'ec2-net-utils',
+                'ec2-utils'
+              ]
+            }
+          }
+        }
+        default: {
+          fail("The ${module_name} module is not supported on an ${::operatingsystem} based system.")
+        }
+      }
     }
     default: {
-      fail("The ${module_name} module is not supported on an ${::operatingsystem} distribution.")
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
 
